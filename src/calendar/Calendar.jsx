939d6cd3c6a68636257df411b56e3c7cfd51c8d1
Andrea <andreavlas97@gmail.com>
2022-05-20 22:07:06 +0300

    added calendar component wip

---
+++ src/calendar/Calendar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import CalendarIcon from "react-calendar-icon";
+
+
+const Calendar = () =>{
+    return (
+        <div>
+          <h1>Hello</h1>
+        </div>
+      );
+}
+
+export default Calendar;
